269b79f1249091c5960c359f9d8d4d2d22630cad
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-06-03 09:30:12 +1000

    Created Structure and Enums for BattleEffect Data

---
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.BattleManager/BattleEffect/Data/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssaultBird2454.VPTU.BattleManager.BattleEffect.Data
+{
+    public enum Triggers { }
+    public enum Conditions { }
+    public enum Actions { }
+}
---
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.BattleManager/BattleEffect/EffectData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssaultBird2454.VPTU.BattleManager.BattleEffect
+{
+    public class EffectData
+    {
+        /// <summary>
+        /// Lists all the triggers that will trigger this effect
+        /// </summary>
+        public List<Data.Triggers> Triggers { get; set; }
+
+        /// <summary>
+        /// All the conditions that need to pass to execute the actions
+        /// </summary>
+        public List<Data.Conditions> Conditions { get; set; }
+        /// <summary>
+        /// Defines if all the Conditions need to pass or a mininum of one to execute the actions
+        /// </summary>
+        public bool Conditions_All { get; set; }
+
+        /// <summary>
+        /// All the actions that this effect will execute for this effect
+        /// </summary>
+        public List<Data.Actions> Actions { get; set; }
+    }
+}
