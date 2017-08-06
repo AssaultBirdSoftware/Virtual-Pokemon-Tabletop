e7c960efce446b04a6f739c42bd4ddb2d6c918cf
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-08-06 20:35:57 +1000

    Added a Remove Status Effect Designer

---
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.BattleManager/BattleEffect/Data/UI/StatusEffect_Remove.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AssaultBird2454.VPTU.BattleManager.BattleEffect.Data.UI
+{
+    /// <summary>
+    /// Interaction logic for StatusEffect_Add.xaml
+    /// </summary>
+    public partial class StatusEffect_Remove : UserControl, EffectAction_Designer
+    {
+        public StatusEffect_Remove()
+        {
+            InitializeComponent();
+
+            Status_Effects.ItemsSource = Enum.GetValues(typeof(VPTU.BattleManager.Data.Status_Afflictions));
+        }
+
+        public void Load(dynamic Data)
+        {
+            RemoveStatusEffect EffectData = (RemoveStatusEffect)Data;
+
+            Status_Effects.SelectedItem = EffectData.StatusEffect;
+        }
+
+        public void Save(dynamic Data)
+        {
+            RemoveStatusEffect EffectData = (RemoveStatusEffect)Data;
+
+            EffectData.StatusEffect = (VPTU.BattleManager.Data.Status_Afflictions)Status_Effects.SelectedItem;
+        }
+    }
+}
