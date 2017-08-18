16a41eeaf3d42980e94b8958ef9c971378e40d7e
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-08-19 08:16:11 +1000

    Added the Character Manager Lib, ready for Dev

---
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.CharacterManager/Trainer/TrainerCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterManager.Trainer
+{
+    public class TrainerCharacter
+    {
+        public string Name { get; set; }
+
+        public string Personality { get; set; }
+
+
+    }
+}
--- Virtual Pokemon Tabletop/AssaultBird2454.VPTU.Pokedex/Entity/EntityGroup.cs
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.Pokedex/Entity/EntityGroup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AssaultBird2454.VPTU.BattleManager.Entity
+namespace AssaultBird2454.VPTU.Pokedex.Entity
 {
     public class EntityGroup
     {
