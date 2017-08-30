fea371f19070c671234aaf0d26e19c16e0e4011f
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-08-30 18:31:41 +1000

    Added Event Handelers

--- Virtual Pokemon Tabletop/AssaultBird2454.VPTU.Networking/Server/TCP/Command Handeler/CommandHandeler.cs
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.Networking/Server/TCP/Command Handeler/CommandHandeler.cs
@@ -16,9 +16,21 @@ namespace AssaultBird2454.VPTU.Networking.Server.Command_Handeler
         public CommandNameTakenException(string Name) : base("The Command Name \"" + Name + "\" is taken... Use another name for that command") { }
     }
     #endregion
+    #region Delegates
+    public delegate void CommandEvent(string Command, string Callback = "");
+    #endregion
 
     public class Server_CommandHandeler
     {
+        /// <summary>
+        /// An event that is fired when a command is registered
+        /// </summary>
+        public event CommandEvent CommandRegistered;
+        /// <summary>
+        /// An event that is fired when a command is unregistered
+        /// </summary>
+        public event CommandEvent CommandUnRegistered;
+
         private Dictionary<string, object> Commands;
 
         public Server_CommandHandeler()
@@ -41,6 +53,7 @@ namespace AssaultBird2454.VPTU.Networking.Server.Command_Handeler
             }
 
             Commands.Add(CommandName, new Command(CommandName, typeof(T), Callback));// Add the command to the command list
+            CommandRegistered?.Invoke(CommandName, Callback.ToString());// Fire Event
         }
 
         /// <summary>
@@ -58,6 +71,7 @@ namespace AssaultBird2454.VPTU.Networking.Server.Command_Handeler
             try
             {
                 Commands.Remove(Name);// Removes the command
+                CommandUnRegistered?.Invoke(Name);
             }
             catch { /* Does not exist, dont not matter */ }
         }
