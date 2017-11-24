e8b7bf4a9cc908d625e7d3752b3daf64bc5f21a2
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-11-25 06:58:53 +1000

    Commented and fixed a bug that may not display the pokedex list correctly

--- Virtual Pokemon Tabletop/AssaultBird2454.VPTU.Client/UI/Pokedex.xaml.cs
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.Client/UI/Pokedex.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,28 +26,57 @@ namespace AssaultBird2454.VPTU.Client.UI
             InitializeComponent();
         }
 
+        /// <summary>
+        /// When the user wants to update the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ToolBar_Reload_Click(object sender, RoutedEventArgs e)
         {
-            List.Items.Clear();
-
-            Program.ClientInstance.Client.SendData(new VPTU.Server.Instances.CommandData.Pokedex.Pokedex_Pokemon_GetList());
+            Program.ClientInstance.Client.SendData(new VPTU.Server.Instances.CommandData.Pokedex.Pokedex_Pokemon_GetList());// Gets the list again
         }
 
+        Thread UpdateThread;
+        /// <summary>
+        /// Updates the list of pokemon
+        /// </summary>
+        /// <param name="Pokemon">The Pokedex List to use</param>
         public void Pokedex_Pokemon_Get_Executed(List<VPTU.Pokedex.Pokemon.PokemonData> Pokemon)
         {
-
-            foreach (VPTU.Pokedex.Pokemon.PokemonData data in Pokemon)
+            try
             {
-                List.Dispatcher.Invoke(new Action(() => List.Items.Add(data)));
+                UpdateThread.Abort();
+                UpdateThread = null;
             }
+            catch { /* Dont Care */ }
+
+            UpdateThread = new Thread(new ThreadStart(() =>// Runs the update in a thread that can be aborted if a new list is retrieved
+            {
+                List.Dispatcher.Invoke(new Action(() =>// Runs the update on the correct thread
+                {
+                    List.Items.Clear();// Clears the ListView
+
+                    foreach (VPTU.Pokedex.Pokemon.PokemonData data in Pokemon)// Runs through the Pokedex List
+                    {
+                        List.Dispatcher.Invoke(new Action(() => List.Items.Add(data)));// And adds it to the ListView
+                    }
+                }));
+            }));
+            UpdateThread.IsBackground = true;
+            UpdateThread.Start();
         }
 
+        /// <summary>
+        /// Gets the pokemon selected and opens a Pokedex Card for it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void List_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            Program.ClientInstance.Client.SendData(new VPTU.Server.Instances.CommandData.Pokedex.Pokedex_Pokemon()
+            Program.ClientInstance.Client.SendData(new VPTU.Server.Instances.CommandData.Pokedex.Pokedex_Pokemon()// Gets the Pokemon Selected
             {
-                Command = "Pokedex_Pokemon_Get",
-                DexID = ((VPTU.Pokedex.Pokemon.PokemonData)List.SelectedItems[0]).Species_DexID
+                Command = "Pokedex_Pokemon_Get",// Sets the command
+                DexID = ((VPTU.Pokedex.Pokemon.PokemonData)List.SelectedItems[0]).Species_DexID// Sets the Pokemon ID to get
             });
         }
     }
