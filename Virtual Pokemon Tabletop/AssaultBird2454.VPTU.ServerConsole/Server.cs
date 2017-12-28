635b924a106636884838ea05a6cf41369071af68
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-12-28 10:30:14 +1000

    Server UI Changes

--- Virtual Pokemon Tabletop/AssaultBird2454.VPTU.ServerConsole/Server.cs
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.ServerConsole/Server.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AssaultBird2454.VPTU.Server.Instances;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +14,8 @@ namespace AssaultBird2454.VPTU.ServerConsole
         public string Server_Name { get; set; }
         public int Server_Port { get; set; }
         public string SaveFile { get; set; }
+
+        [JsonIgnore]
+        public ServerInstance Server_Instance;
     }
 }
