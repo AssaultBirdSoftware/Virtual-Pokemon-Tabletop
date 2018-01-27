6fb5ecf333a71ae71c22ba3da05000181cf10ea6
Tasman Leach <AssaultBird2454@users.noreply.github.com>
2018-01-27 14:06:10 +1000

    Moved Valid Tags

--- contributors.js
+++ contributors.js
@@ -2,6 +2,22 @@
  * When you contribute to Virtual-Pokemon-Tabletop, please add your info to this. You don't need to include `firstName`,
  * or any of the links if you don't want to, but include everything else.
  */
+
+/** 
+ * Valid Tags
+ * -=-=-=-=-=-=-=-=- Dont Use These Tags -=-=-=-=-=-=-=-=-
+ * NOTE: These tags will get applied to you by the Head Developer's and Project Managers...
+ * project-manager      -> User is the project manager
+ * head-developer       -> User is a head developer
+ *
+ *-=-=-=-=-=-=-=-=-=-= Use These Tags =-=-=-=-=-=-=-=-=-=-
+ * developer            -> User contributes code to fix bugs, issues and even features
+ * debug-qa             -> User is testing the software frequently to find bugs
+ * graphics             -> User has provided some graphical assets for use in this project
+ * game-master-advice   -> User has offered advice relating to the PTU system during development.
+ *                         You may add this tag also if your an experienced GM
+ * support              -> User is able to offer technical support with this project
+ */
 initContributors([
     {
         username: "AssualtBird2454",
@@ -49,19 +65,3 @@ initContributors([
     
     
 ]);
-
-/** 
- * Valid Tags
- * -=-=-=-=-=-=-=-=- Dont Use These Tags -=-=-=-=-=-=-=-=-
- * NOTE: These tags will get applied to you by the Head Developer's and Project Managers...
- * project-manager      -> User is the project manager
- * head-developer       -> User is a head developer
- *
- *-=-=-=-=-=-=-=-=-=-= Use These Tags =-=-=-=-=-=-=-=-=-=-
- * developer            -> User contributes code to fix bugs, issues and even features
- * debug-qa             -> User is testing the software frequently to find bugs
- * graphics             -> User has provided some graphical assets for use in this project
- * game-master-advice   -> User has offered advice relating to the PTU system during development.
- *                         You may add this tag also if your an experienced GM
- * support              -> User is able to offer technical support with this project
- */
