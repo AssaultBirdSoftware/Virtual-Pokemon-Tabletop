bbd6f11f2c7bfd9c507b83f093a5876c68f16f19
Tasman Leach <AssaultBird2454@gmail.com>
2018-02-03 21:08:13 +1000

    Created the UI elements for the new settings interface.

---
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.SaveEditor/UI/Settings/Settings Panel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AssaultBird2454.VPTU.SaveEditor.UI.Settings
+{
+    /// <summary>
+    /// Interaction logic for Settings_Panel.xaml
+    /// </summary>
+    public partial class Settings_Panel : UserControl
+    {
+        public Settings_Panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
