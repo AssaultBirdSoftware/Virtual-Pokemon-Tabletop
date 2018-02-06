9c701bf658a5b4859dd0843547c86f40e54a9bff
Tasman Leach <AssaultBird2454@gmail.com>
2018-02-06 18:47:02 +1000

    Implemented a Tooltip for the resource Manager Tab

--- Virtual Pokemon Tabletop/AssaultBird2454.VPTU.SaveEditor/MainWindow.xaml.cs
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU.SaveEditor/MainWindow.xaml.cs
@@ -8,7 +8,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using AssaultBird2454.VPTU.Authentication_Manager.Data;
 using AssaultBird2454.VPTU.EntitiesManager;
 using AssaultBird2454.VPTU.EntitiesManager.Pokemon;
@@ -855,9 +857,43 @@ namespace AssaultBird2454.VPTU.SaveEditor
                 ResourceManager_List.Items.Clear();
 
                 if (ResourceManager_SearchRes_Images.IsChecked == true)
+                {
                     foreach (var res in SaveManager.SaveData.ImageResources)
+                    {
                         if (res.Name.ToLower().Contains(ResourceManager_SearchRes_Search.Text.ToLower()))
-                            ResourceManager_List.Items.Add(res);
+                        {
+                            System.Windows.Controls.ListViewItem lvi = new System.Windows.Controls.ListViewItem();
+                            lvi.Content = res;
+                            lvi.ToolTip = new StackPanel();
+                            lvi.ToolTipClosing += new ToolTipEventHandler((sender, e) =>
+                            {
+                                ((StackPanel)lvi.ToolTip).Children.Clear();
+                            });
+
+                            lvi.ToolTipOpening += new ToolTipEventHandler((sender, e) =>
+                            {
+                                ((StackPanel)lvi.ToolTip).Children.Clear();
+
+                                ((StackPanel)lvi.ToolTip).Children.Add(new Image()
+                                {
+                                    Source = Imaging.CreateBitmapSourceFromHBitmap(SaveManager.LoadImage(res.ID).GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
+                                    MaxHeight = 150,
+                                    MaxWidth = 150,
+                                    Stretch = Stretch.Uniform
+                                }
+                                );
+
+                                ((StackPanel)lvi.ToolTip).Children.Add(
+                                new TextBlock()
+                                {
+                                    Text = "ID: " + res.ID + "\nName: " + res.Name
+                                }
+                                );
+                            });
+                            ResourceManager_List.Items.Add(lvi);
+                        }
+                    }
+                }
             }
             catch
             {
