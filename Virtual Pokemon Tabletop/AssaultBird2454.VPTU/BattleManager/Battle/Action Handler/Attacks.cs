cfc614fc88ad8f3af60f7f7ba3ceb558439b617f
Tasman Leach <AssaultBird2454@gmail.com>
2018-02-23 07:10:48 +1000

    Started implementing dealing attack damage

--- Virtual Pokemon Tabletop/AssaultBird2454.VPTU/BattleManager/Battle/Action Handler/Attacks.cs
+++ Virtual Pokemon Tabletop/AssaultBird2454.VPTU/BattleManager/Battle/Action Handler/Attacks.cs
@@ -3,30 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AssaultBird2454.VPTU.BattleManager.Battle.Action_Handler
 {
     public static class Attack
     {
-        public static void Invoke_AoE_Attack(Pokedex.Moves.MoveData Move, List<EntitiesManager.Entry> Targets, EntitiesManager.Entry User)
+        public static void Invoke_AoE_Attack(Pokedex.Moves.MoveData Move, List<EntitiesManager.Entities> Targets, EntitiesManager.Entities User)
         {
             VPTU.BattleManager.Effect.Effect_Handler handle = new BattleManager.Effect.Effect_Handler(@"C:\Users\Assau\Desktop\Effect LUA Files\Moves\Attack_Order.lua", true);
 
-            int AC = RNG.Generators.RNG.GenerateNumber(20);
-            int DB = RNG.Generators.RNG.GenerateNumber(20);
+            int AC = RNG.Generators.RNG.GenerateNumber(20);// Rolls the AC
+            int DB = RNG.Generators.RNG.GenerateNumber(20);// Rolls the DB
 
-            foreach (EntitiesManager.Entry e in Targets)
+            foreach (EntitiesManager.Entities e in Targets)
             {
-                handle.Set_GlobalVariable("Cancel", false);
+                handle.Set_GlobalVariable("Cancel", false);// Creates a global variable to cancel the attack
+                handle.Set_GlobalVariable("Hit", false);// Set Hit
+                handle.Set_GlobalVariable("Damage", null);// Damage to be delt
+                handle.Set_GlobalVariable("Crit", false);// Set not critical hit
+                handle.Set_GlobalVariable("AC", AC);// Set AC
+                handle.Set_GlobalVariable("DB", DB);// Set DB
 
-                if (AC == 20)
-                    handle.Set_GlobalVariable("Crit", true);
+                if (Move.Move_Class == Data.MoveClass.Physical)
+                {
+                    if (e.Evasion_Physical >= e.Evasion_Speed)
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Physical);// DC for move with physical evasion
+                    }
+                    else
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Speed);// DC for move with speed evasion
+                    }
+                }
+                else if (Move.Move_Class == Data.MoveClass.Special)
+                {
+                    if (e.Evasion_Special >= e.Evasion_Speed)
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Special);// DC for move with Special evasion
+                    }
+                    else
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Speed);// DC for move with speed evasion
+                    }
+                }
+                else if (Move.Move_Class == Data.MoveClass.Status)
+                {
+                    handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Speed);// DC for move with speed evasion
+                }
                 else
-                    handle.Set_GlobalVariable("Crit", false);
+                {
+                    // What to do if move is static?
+                }
 
-                handle.Set_GlobalVariable("AC", AC);
-                handle.Set_GlobalVariable("DB", DB);
-                handle.Get_Trigger("Attack_Invoked").Call(null, e, User);
+                if (AC == 20)// If Critical hit
+                    handle.Set_GlobalVariable("Crit", true);// Set Critical Hit
+
+                handle.Get_Trigger("Pre_Attack_Invoked").Call(null, e, User);// Invoke effects to be executed before damage is delt
 
                 if ((bool)handle.Get_GlobalVariable("Crit"))
                 {
@@ -40,29 +73,64 @@ namespace AssaultBird2454.VPTU.BattleManager.Battle.Action_Handler
                 if (!(bool)handle.Get_GlobalVariable("Cancel"))
                 {
                     // Continue
-                }
+
+                    MessageBox.Show("");
+                    handle.Get_Trigger("Post_Attack_Invoked").Call(null, e, User);// Invoke effects to be executed before damage is delt
+                }// Check if the attack is not canceled
             }
         }
 
-        public static void Invoke_Range_Attack(Pokedex.Moves.MoveData Move, List<EntitiesManager.Entry> Targets, EntitiesManager.Entry User)
+        public static void Invoke_Range_Attack(Pokedex.Moves.MoveData Move, List<EntitiesManager.Entities> Targets, EntitiesManager.Entities User)
         {
             VPTU.BattleManager.Effect.Effect_Handler handle = new BattleManager.Effect.Effect_Handler(@"C:\Users\Assau\Desktop\Effect LUA Files\Moves\Attack_Order.lua", true);
 
-            foreach (EntitiesManager.Entry e in Targets)
+            foreach (EntitiesManager.Entities e in Targets)// for each target
             {
-                handle.Set_GlobalVariable("Cancel", false);
+                int AC = RNG.Generators.RNG.GenerateNumber(20);// Rolls the AC
+                int DB = RNG.Generators.RNG.GenerateNumber(20);// Rolls the DB
 
-                int AC = RNG.Generators.RNG.GenerateNumber(20);
-                int DB = RNG.Generators.RNG.GenerateNumber(20);
+                handle.Set_GlobalVariable("Cancel", false);// Creates a global variable to cancel the attack
+                handle.Set_GlobalVariable("Hit", false);// Set Hit
+                handle.Set_GlobalVariable("Damage", null);// Damage to be delt
+                handle.Set_GlobalVariable("Crit", false);// Set not critical hit
+                handle.Set_GlobalVariable("AC", AC);// Set AC
+                handle.Set_GlobalVariable("DB", DB);// Set DB
 
-                if (AC == 20)
-                    handle.Set_GlobalVariable("Crit", true);
+                if (Move.Move_Class == Data.MoveClass.Physical)
+                {
+                    if (e.Evasion_Physical >= e.Evasion_Speed)
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Physical);// DC for move with physical evasion
+                    }
+                    else
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Speed);// DC for move with speed evasion
+                    }
+                }
+                else if (Move.Move_Class == Data.MoveClass.Special)
+                {
+                    if (e.Evasion_Special >= e.Evasion_Speed)
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Special);// DC for move with Special evasion
+                    }
+                    else
+                    {
+                        handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Speed);// DC for move with speed evasion
+                    }
+                }
+                else if (Move.Move_Class == Data.MoveClass.Status)
+                {
+                    handle.Set_GlobalVariable("DC_AC", Move.Move_Accuracy + e.Evasion_Speed);// DC for move with speed evasion
+                }
                 else
-                    handle.Set_GlobalVariable("Crit", false);
+                {
+                    // What to do if move is static?
+                }
 
-                handle.Set_GlobalVariable("AC", AC);
-                handle.Set_GlobalVariable("DB", DB);
-                handle.Get_Trigger("Attack_Invoked").Call(null, e, User);
+                if (AC == 20)// If Critical hit
+                    handle.Set_GlobalVariable("Crit", true);// Set Critical Hit
+
+                handle.Get_Trigger("Pre_Attack_Invoked").Call(null, e, User);// Invoke effects to be executed before damage is delt
 
                 if ((bool)handle.Get_GlobalVariable("Crit"))
                 {
@@ -76,7 +144,8 @@ namespace AssaultBird2454.VPTU.BattleManager.Battle.Action_Handler
                 if (!(bool)handle.Get_GlobalVariable("Cancel"))
                 {
                     // Continue
-                }
+                    handle.Get_Trigger("Post_Attack_Invoked").Call(null, e, User);// Invoke effects to be executed before damage is delt
+                }// Check if the attack is not canceled
             }
         }
     }
